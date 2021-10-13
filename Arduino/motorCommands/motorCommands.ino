60e4caa3c8d7ee9e9a3085933cdcc85a71c4dfd9
Ian Eykamp <ian@eykamp.com>
2021-10-12 20:45:18 -0400

    update

--- Arduino/motorCommands/motorCommands.ino
+++ Arduino/motorCommands/motorCommands.ino
@@ -1,46 +1,36 @@
-/*
-This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
-It won't work with v1.x motor shields! Only for the v2's with built in PWM
-control
-
-For use with the Adafruit Motor Shield v2
----->	http://www.adafruit.com/products/1438
-*/
-
 #include <Adafruit_MotorShield.h>
 
 // Create the motor shield object with the default I2C address
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *myMotorLeft = AFMS.getMotor(1);
 Adafruit_DCMotor *myMotorRight = AFMS.getMotor(2);
-// You can also make another motor on port M2
-//Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
 const int numSensors = 2;
 const int sensorPins[numSensors] = {A1, A2};
 int sensorValues[numSensors];
 
+const int THRESH = 400;
+const int SPEED = 127;
+
 void setup()
 {
   Serial.begin(9600); // set up Serial library at 9600 bps
-  // Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
 
-  // if (!AFMS.begin())
-  // { // create with the default frequency 1.6KHz
-  //   // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
-  //   Serial.println("Could not find Motor Shield. Check wiring.");
-  //   while (1)
-  //     ;
-  // }
-  // Serial.println("Motor Shield found.");
+  if (!AFMS.begin())
+  { // create with the default frequency 1.6KHz
+    // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+    while (1)
+      ;
+  }
+  Serial.println("Motor Shield found.");
 
-  // // turn on motor
-  // myMotorLeft->run(RELEASE);
-  // myMotorRight->run(RELEASE);
+  // turn on motor
+  myMotorLeft->run(RELEASE);
+  myMotorRight->run(RELEASE);
 }
 
 void readSensors()
@@ -49,6 +39,11 @@ void readSensors()
   {
     sensorValues[sensor] = analogRead(sensorPins[sensor]);
   }
+
+  Serial.print("Senosor 1");
+  Serial.print(sensorValues[0]);
+  Serial.print("   Senosor 2");
+  Serial.println(sensorValues[1]);
 }
 
 void driveForward(int speed)
@@ -83,11 +78,15 @@ void stop()
 
 void loop()
 {
-  // int * sensorValues[2];
   readSensors();
 
-  Serial.println("Senosor 1");
-  Serial.println(sensorValues[0]);
-  Serial.println("Senosor 2");
-  Serial.println(sensorValues[1]);
+  if (sensorValues[0] < THRESH && sensorValues[1] < THRESH) {
+    driveForward(SPEED);
+  }
+  if (sensorValues[0] > THRESH && sensorValues[1] < THRESH) {
+    turnRight(0, 0, SPEED);
+  }
+  if (sensorValues[0] < THRESH && sensorValues[1] > THRESH) {
+    turnLeft(0, 0, SPEED);
+  }
 }
