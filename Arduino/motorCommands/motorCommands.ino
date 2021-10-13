d5e8b321f36b8a5fffcb117f86b7a9fb134c19f6
Ian Eykamp <ian@eykamp.com>
2021-10-12 20:09:04 -0400

    add sensor read function

--- Arduino/motorCommands/motorCommands.ino
+++ Arduino/motorCommands/motorCommands.ino
@@ -20,59 +20,74 @@ Adafruit_DCMotor *myMotorRight = AFMS.getMotor(2);
 // You can also make another motor on port M2
 //Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(2);
 
-void setup() {
-  Serial.begin(9600);           // set up Serial library at 9600 bps
-  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+const int numSensors = 2;
+const int sensorPins[numSensors] = {A1, A2};
+int sensorValues[numSensors];
 
-  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
-    Serial.println("Could not find Motor Shield. Check wiring.");
-    while (1);
+void setup()
+{
+  Serial.begin(9600); // set up Serial library at 9600 bps
+  // Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+  // if (!AFMS.begin())
+  // { // create with the default frequency 1.6KHz
+  //   // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+  //   Serial.println("Could not find Motor Shield. Check wiring.");
+  //   while (1)
+  //     ;
+  // }
+  // Serial.println("Motor Shield found.");
+
+  // // turn on motor
+  // myMotorLeft->run(RELEASE);
+  // myMotorRight->run(RELEASE);
+}
+
+void readSensors()
+{
+  for (int sensor = 0; sensor < numSensors; sensor++)
+  {
+    sensorValues[sensor] = analogRead(sensorPins[sensor]);
   }
-  Serial.println("Motor Shield found.");
-
-  // Set the speed to start, from 0 (off) to 255 (max speed)
-  myMotorLeft->setSpeed(150);
-  myMotorRight->setSpeed(150);
-  myMotorLeft->run(FORWARD);
-  myMotorRight->run(FORWARD);
-  // turn on motor
-  myMotorLeft->run(RELEASE);
-  myMotorRight->run(RELEASE);
 }
 
-void loop() {
-  uint8_t i;
-
-  Serial.print("tick");  
-  Serial.print("tech");
-   delay(1000);
-}
-
-
-void driveForward(int speed) {
+void driveForward(int speed)
+{
   myMotorLeft->run(FORWARD);
   myMotorRight->run(FORWARD);
   myMotorLeft->setSpeed(speed);
   myMotorRight->setSpeed(speed);
-
 }
 
-void turnLeft(int ang, int radius, int speed) {
+void turnLeft(int ang, int radius, int speed)
+{
   myMotorLeft->run(FORWARD);
   myMotorRight->run(FORWARD);
   myMotorLeft->setSpeed(speed);
-  myMotorRight->setSpeed(3*speed);
+  myMotorRight->setSpeed(3 * speed);
 }
 
-void turnRight(int ang, int radius, int speed) {
+void turnRight(int ang, int radius, int speed)
+{
   myMotorLeft->run(FORWARD);
   myMotorRight->run(FORWARD);
-  myMotorLeft->setSpeed(3*speed);
+  myMotorLeft->setSpeed(3 * speed);
   myMotorRight->setSpeed(speed);
 }
 
-void stop(){
+void stop()
+{
   myMotorLeft->run(RELEASE);
   myMotorRight->run(RELEASE);
+}
+
+void loop()
+{
+  // int * sensorValues[2];
+  readSensors();
+
+  Serial.println("Senosor 1");
+  Serial.println(sensorValues[0]);
+  Serial.println("Senosor 2");
+  Serial.println(sensorValues[1]);
 }
